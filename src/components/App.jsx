415a87c46c2c87d52decaf775df6a6b6db665d58
Mateusz <mateuszszczykutowicz@wp.pl>
2021-03-16 12:51:42 +0100

    Initial project

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/App.module.scss";
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello React</h1>
+            <div className={styles.test}>Test</div>
+        </>
+    );
+};
+
+export default App;
